2055827bd56b210b8b57a5927aece9f18c3d84c0
blazzingkiller <uri.zakaior@post.runi.ac.il>
2024-11-14 11:22:57 -0600

    Update TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -4,10 +4,8 @@ public class TimeFormat {
         int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 
         String period = (hours >= 12) ? " PM" : " AM";
-        
         int displayHour = (hours % 12 == 0) ? 12 : hours % 12;
-
-		String formattedMinutes = (minutes < 10) ? "0" + minutes : String.valueOf(minutes);
+	String formattedMinutes = (minutes < 10) ? "0" + minutes : String.valueOf(minutes);
 
         System.out.println( displayHour + ":" + formattedMinutes + period);
     }
