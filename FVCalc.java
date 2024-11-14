0332662e4188da776ab93c3456c46615da793a59
blazzingkiller <uri.zakaior@post.runi.ac.il>
2024-11-14 11:22:24 -0600

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -3,10 +3,8 @@ public class FVCalc {
 	int currentValue = Integer.parseInt(args[0]); 
 	double irate = Double.parseDouble(args[1]);
 	int n = Integer.parseInt(args[2]); 
-
 	double rate = (irate/100);
 	double futureValue = (currentValue * (Math.pow((1 + rate),n )));
-
 	System.out.println("After " + n + " years, $" + currentValue + " saved at " + irate + "% will yield $" + ((int) futureValue));
 	}
 }
