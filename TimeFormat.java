47a43d4e6bc65650b86b4625ad4df071549c73c8
blazzzingkiller <uri.zakaior@post.runi.ac.il>
2024-11-14 19:28:39 -0600

    Initial commit

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,5 +1,5 @@
 public class TimeFormat {
-    public static void main(String[] args) {
+
         int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
         int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 
